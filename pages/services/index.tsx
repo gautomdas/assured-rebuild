71d50c827214f732db0e042f8a100ea6b0d362ed
Gautom Das <gdas@umd.edu>
2021-06-27 02:28:18 -0400

    Working

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -57,6 +57,62 @@ export default function Home({ prices }: IProps) {
           </div>
 
           <div className="flex flex-col lg:flex-row items-center lg:space-x-12">
+            <div className="flex-1 rounded shadow-xl px-6 py-8 lg:px-10 lg:py-10">
+              <div className="flex-grow text-2xl font-bold lg:text-3xl  ml-3">
+                Class of 2022 Promo Package
+              </div>
+              <div className="flex mt-4">
+                <div className="flex-none w-5 h-5 ...">
+                  <img
+                    className="float-right h-8"
+                    src="/check.svg"
+                    alt="Assured Global Consulting Logo"
+                  />
+                </div>
+                <div className="flex-grow text-base lg:text-2xl  ml-3">
+                  Personal Statement Review
+                </div>
+              </div>
+              <div className="flex mt-4">
+                <div className="flex-none w-5 h-5 ...">
+                  <img
+                    className="float-right h-8"
+                    src="/check.svg"
+                    alt="Assured Global Consulting Logo"
+                  />
+                </div>
+                <div className="flex-grow text-base lg:text-2xl  ml-3">
+                  Complete Supplemental Essay Review*
+                </div>
+              </div>
+              <div className="flex mt-4">
+                <div className="flex-none w-5 h-5 ...">
+                  <img
+                    className="float-right h-8"
+                    src="/check.svg"
+                    alt="Assured Global Consulting Logo"
+                  />
+                </div>
+                <div className="flex-grow text-base lg:text-2xl  ml-3">
+                  Mock Interview (1 session)
+                </div>
+              </div>
+              <div className=" text-base lg:text-1xl mt-5 text-gray-600">
+                * 1 full round of edits on all supplemental essays/short answers
+                for 1 school.
+              </div>
+              <div className="mt-5 lg:mt-10 mx-auto lg:text-center text-base font-bold lg:text-4xl  ml-3">
+                349.99 USD
+              </div>
+
+              <button
+                onClick={() => onClick("price_1Itcq2J8MpFEqO9MOclh83kh")}
+                className="content-center bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-5 py-2 px-4 border border-gray-400 rounded shadow"
+              >
+                Purchase
+              </button>
+            </div>
+
             <div className="flex-1 bg-pink rounded shadow-xl px-6 py-8 lg:px-10 lg:py-10 mt-10">
               <div className="flex-grow text-white text-2xl font-bold lg:text-3xl  ml-3">
                 Premium Promo Package
@@ -71,7 +127,7 @@ export default function Home({ prices }: IProps) {
                 </div>
                 <div className="flex-grow text-white text-base lg:text-2xl  ml-3">
                   Personal Statement Review with{" "}
-                  <b className="font-black">1 full rounds</b> of edits.
+                  <b className="font-black">2 full rounds</b> of edits.
                 </div>
               </div>
 
@@ -84,7 +140,7 @@ export default function Home({ prices }: IProps) {
                   />
                 </div>
                 <div className="flex-grow text-white text-base lg:text-2xl  ml-3">
-                  Complete Supplemental Essay Review*
+                  Complete Supplemental Essay Review
                 </div>
               </div>
 
@@ -98,76 +154,36 @@ export default function Home({ prices }: IProps) {
                 </div>
                 <div className="flex-grow text-white text-base lg:text-2xl  ml-3">
                   Mock Interview (1 session)
+                </div>
+              </div>
+
+              <div className="flex mt-4">
+                <div className="flex-none w-5 h-5 ...">
+                  <img
+                    className="float-right h-8"
+                    src="/check_white.svg"
+                    alt="Assured Global Consulting Logo"
+                  />
+                </div>
+                <div className="flex-grow text-white text-base lg:text-2xl  ml-3">
+                  Bonus: A-La-Carte Consulting (1 hour)
                 </div>
               </div>
 
               <div className=" text-base lg:text-1xl mt-5 text-gray-100 ">
                 * 1 full round of edits on all supplemental essays/short answers
-                for <b className="font-bold">3 schools.</b>
+                for <b className="font-bold">2 schools.</b>
               </div>
 
               <div className="mt-5 lg:mt-10 mx-auto lg:text-center text-white text-base font-bold lg:text-4xl  ml-3">
-                429.99 USD
+                549.99 USD
               </div>
 
               <button
-                onClick={() => onClick("price_1J6pUoJ8MpFEqO9Mu469P53u")}
+                onClick={() => onClick("price_1ItcqlJ8MpFEqO9MCH4yaabU")}
                 className="content-center bg-pink hover:bg-pink-100 text-white font-semibold mt-5 py-2 px-4 border border-white rounded shadow"
               >
                 Purchase
-              </button>
-            </div>
-
-            <div className="flex-1 rounded shadow-xl px-6 py-8 lg:px-10 lg:py-10">
-              <div className="flex-grow text-2xl text-gray-400 font-bold lg:text-3xl  ml-3">
-                Class of 2022 Promo Package
-              </div>
-              <div className="flex mt-4">
-                <div className="flex-none w-5 h-5 ...">
-                  <img
-                    className="float-right h-8"
-                    src="/check.svg"
-                    alt="Assured Global Consulting Logo"
-                  />
-                </div>
-                <div className="text-gray-400 flex-grow text-base lg:text-2xl  ml-3">
-                  Personal Statement Review
-                </div>
-              </div>
-              <div className="flex mt-4">
-                <div className="flex-none w-5 h-5 ...">
-                  <img
-                    className="float-right h-8"
-                    src="/check.svg"
-                    alt="Assured Global Consulting Logo"
-                  />
-                </div>
-                <div className="text-gray-400 flex-grow text-base lg:text-2xl  ml-3">
-                  Complete Supplemental Essay Review*
-                </div>
-              </div>
-              <div className="flex mt-4">
-                <div className="flex-none w-5 h-5 ...">
-                  <img
-                    className="float-right h-8"
-                    src="/check.svg"
-                    alt="Assured Global Consulting Logo"
-                  />
-                </div>
-                <div className="text-gray-400 flex-grow text-base lg:text-2xl  ml-3">
-                  Mock Interview (1 session)
-                </div>
-              </div>
-              <div className=" text-gray-400 text-base lg:text-1xl mt-5">
-                * 1 full round of edits on all supplemental essays/short answers
-                for 1 school.
-              </div>
-              <div className="text-gray-400 mt-5 lg:mt-10 mx-auto lg:text-center text-base font-bold lg:text-4xl  ml-3">
-                349.99 USD
-              </div>
-
-              <button className="content-center bg-white hover:bg-gray-100 text-gray-400 font-semibold mt-5 py-2 px-4 border border-gray-400 rounded shadow">
-                Sold Out
               </button>
             </div>
           </div>
@@ -180,8 +196,7 @@ export default function Home({ prices }: IProps) {
               <div className="flex-1 content-center">
                 <div className="rounded shadow-xl px-10 py-10">
                   <div className="flex-grow text-base font-bold lg:text-3xl  ml-3">
-                    Personal Statement Review with{" "}
-                    <b className="font-black">1 full rounds</b> of edits.
+                    Personal Statement Review
                   </div>
                   <div className="flex mt-4">
                     <div className="flex-none w-5 h-5 ...">
@@ -243,7 +258,7 @@ export default function Home({ prices }: IProps) {
                   </div>
 
                   <button
-                    onClick={() => onClick("price_1ItcngJ8MpFEqO9MOi3VQrAs")}
+                    onClick={() => onClick("price_1Itzv4J8MpFEqO9MsA2N4GCC")}
                     className="content-center bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-5 py-2 px-4 border border-gray-400 rounded shadow"
                   >
                     Purchase
@@ -256,7 +271,6 @@ export default function Home({ prices }: IProps) {
                   <div className="flex-grow text-base font-bold lg:text-3xl  ml-3">
                     Supplemental Essay Review
                   </div>
-
                   <div className="flex mt-4">
                     <div className="flex-none w-5 h-5 ...">
                       <img
@@ -266,14 +280,12 @@ export default function Home({ prices }: IProps) {
                       />
                     </div>
                     <div className="flex-grow text-base lg:text-2xl  ml-3">
-                      When purchasing,{" "}
-                      <b>
-                        make sure to enter the number of words in the quantity
-                        of the supplemental essays.{" "}
-                      </b>
+                      See pricing{" "}
+                      <a className="text-pink" href="#prices">
+                        below
+                      </a>
                     </div>
                   </div>
-
                   <div className="flex mt-4">
                     <div className="flex-none w-5 h-5 ...">
                       <img
@@ -286,7 +298,6 @@ export default function Home({ prices }: IProps) {
                       Must include prompt within the text of the submitted file.
                     </div>
                   </div>
-
                   <div className="flex mt-4">
                     <div className="flex-none w-5 h-5 ...">
                       <img
@@ -313,11 +324,47 @@ export default function Home({ prices }: IProps) {
                     </div>
                   </div>
                   <div className="mt-5 mx-auto text-center text-base font-bold lg:text-3xl  ml-3">
-                    0.14 USD per word
+                    119.99 USD or Less
                   </div>
 
                   <button
-                    onClick={() => onClick("price_1J6qPPJ8MpFEqO9M0xBfu6IX")}
+                    onClick={() => onClick("price_1ItcoRJ8MpFEqO9MUh1LiHtE")}
+                    className="content-center bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-5 py-2 px-4 border border-gray-400 rounded shadow"
+                  >
+                    Purchase
+                  </button>
+                </div>
+              </div>
+
+              <div className="flex-1 content-center">
+                <div className="rounded shadow-xl px-10 py-10">
+                  <div className="flex-grow text-base font-bold lg:text-3xl  ml-3">
+                    A-La-Carte Consulting
+                  </div>
+                  <div className="flex mt-4">
+                    <div className="flex-none w-5 h-5 ...">
+                      <img
+                        className="float-right h-8"
+                        src="/check.svg"
+                        alt="Assured Global Consulting Logo"
+                      />
+                    </div>
+                    <div className="flex-grow text-base lg:text-2xl  ml-3">
+                      Students can go over essays, college application
+                      strategies, questions, or anything with us
+                    </div>
+                  </div>
+
+                  <div className="mt-5 mx-auto text-center text-base font-bold lg:text-3xl  ml-3">
+                    39.99 USD*
+                  </div>
+
+                  <div className=" text-base lg:text-1xl mt-5 text-gray-600">
+                    * This is priced for every 30 minute interval.
+                  </div>
+
+                  <button
+                    onClick={() => onClick("price_1ItcpPJ8MpFEqO9MLpzRXRjA")}
                     className="content-center bg-white hover:bg-gray-100 text-gray-800 font-semibold mt-5 py-2 px-4 border border-gray-400 rounded shadow"
                   >
                     Purchase
@@ -356,6 +403,56 @@ export default function Home({ prices }: IProps) {
                 </div>
               </div>
             </div>
+          </div>
+
+          <div
+            id="prices"
+            className="flex-grow text-3xl font-bold lg:text-5xl mt-24 ml-3"
+          >
+            Supplemental Essay Pricing
+          </div>
+          <div className="flex-grow text-base lg:text-2xl mt-10 ml-3"></div>
+          <div className="lg:px-32">
+            <table className="table-fixed">
+              <thead>
+                <tr>
+                  <th className="w-1/2 ...">Word Count</th>
+                  <th className="w-1/2 ...">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>50 (Short Answers)</td>
+                  <td className="pl-10">
+                    Free, but must be included w/ a larger essay.
+                  </td>
+                </tr>
+                <tr className="bg-pink-light">
+                  <td>51 – 250</td>
+                  <td className="pl-10">$19.00 per essay</td>
+                </tr>
+                <tr>
+                  <td>251 – 450</td>
+                  <td className="pl-10">$39.00 per essay</td>
+                </tr>
+                <tr className="bg-pink-light">
+                  <td>451 – 650</td>
+                  <td className="pl-10">$59.00 per essay</td>
+                </tr>
+                <tr>
+                  <td>651 – 850</td>
+                  <td className="pl-10">$79.00 per essay</td>
+                </tr>
+                <tr className="bg-pink-light">
+                  <td>851 – 1000</td>
+                  <td className="pl-10">$99.00 per essay</td>
+                </tr>
+                <tr>
+                  <td>1000+</td>
+                  <td className="pl-10">$119.00 per essay</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
