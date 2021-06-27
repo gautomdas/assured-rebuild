fd436f2e28cbbb5f7d2e9e737ad98cc50185b5f4
Gautom Das <gdas@umd.edu>
2021-06-27 03:00:13 -0400

    Working

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -377,16 +377,16 @@ export default function Home({ prices }: IProps) {
   );
 }
 
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-//     apiVersion: "2020-08-27",
-//   });
+export const getServerSideProps: GetServerSideProps = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2020-08-27",
+  });
 
-//   const prices = await stripe.prices.list({
-//     active: true,
-//     limit: 10,
-//     expand: ["data.product"],
-//   });
+  const prices = await stripe.prices.list({
+    active: true,
+    limit: 10,
+    expand: ["data.product"],
+  });
 
-//   return { props: { prices: prices.data } };
-// };
+  return { props: { prices: prices.data } };
+};
